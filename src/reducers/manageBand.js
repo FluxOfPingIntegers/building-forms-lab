1028ce30c0e1396a9eeea4a9a3416883df121c29
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-11-01 15:27:49 -0400

    commit tree 9f760629c49be152b4250dbdcdfc10b3014b148e

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,5 +1,16 @@
-export default function manageBand(state = {
-  bands: [],
-}, action) {
-  return state
+export default function manageBand(
+  state = {
+    bands: [],
+  }, 
+  action
+) {
+  switch (action.type) {
+    case "ADD_BAND":
+      console.log({ bands: [...state.bands, action.band] });
+
+      return { ...state, bands: [...state.bands, action.band] };
+
+    default:
+      return state
+  }
 };
